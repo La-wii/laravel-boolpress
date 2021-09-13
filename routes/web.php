c3370ceb68748e2de46f4ce95b9aef888289a09b
La-wii <sentilavale@gmail.com>
2021-09-13 16:28:04 +0200

    model + seeder

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('guest.home');
-});
+Route::get('/', 'HomeController@index')->name('Homepage');
 
 Auth::routes();
 
